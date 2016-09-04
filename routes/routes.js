39c70dd4dd2f784c25144dc7ae6714ae01c01737
Mikkel Petersen <mlp2305@gmail.com>
2016-09-04 03:32:56 +0200

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,66 @@
+//logon/off redirects
+Accounts.onLogin(function(user) {
+  FlowRouter.go("main")
+})
+
+Accounts.onLogout(function() {
+  FlowRouter.go("home")
+})
+
+//helper functions
+function checkLogin() {
+  if(!Meteor.userId()) {
+      FlowRouter.go("home")
+  }
+}
+//Groups
+let exposed = FlowRouter.group()
+let loggedIn = FlowRouter.group({
+  triggersEnter:[checkLogin]
+})
+
+
+
+
+exposed.route("/",{
+  name:"home",
+  action() {
+    if(Meteor.userId()) {
+      FlowRouter.go("main")
+    }
+    BlazeLayout.render("Home");
+  }
+})
+loggedIn.route("/main",{
+  name:"main",
+  action() {
+    BlazeLayout.render("Main");
+  }
+})
+
+loggedIn.route("/customers",{
+  name:"customers",
+  action() {
+    BlazeLayout.render("Customers");
+  }
+})
+
+loggedIn.route("/tasks",{
+  name:"tasks",
+  action() {
+      BlazeLayout.render("Tasks");
+  }
+
+  })
+loggedIn.route("/orders",{
+  name:"orders",
+  action() {
+    BlazeLayout.render("Orders");
+  }
+})
+loggedIn.route("/admin",{
+  name:"admin",
+  action() {
+    BlazeLayout.render("Admin");
+  }
+})
